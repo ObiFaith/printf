fc37b0129ae65aaa3c696e280d7846c9c234b2db
Obi Faith <obifth@gmail.com>
2024-01-29 22:07:45 -0800

    Added README.md & main.h files

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+
+#endif
