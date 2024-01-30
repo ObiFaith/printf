33ca104bd4ebd652dc87b6f950434dee6cf738ab
0xKanu <kanuonyedikachi7@gmail.com>
2024-01-31 00:09:53 +0100

    add print_num func

--- main.h
+++ main.h
@@ -9,6 +9,6 @@
 #include <limits.h>
 
 int _printf(const char *format, ...);
-void print_num(int num);
+int print_num(int num);
 
 #endif
--- print_num.c
+++ print_num.c
@@ -1,3 +1,19 @@
 #include "main.h"
 /**
- * print_num - 
+ * print_num - returns the format specifier.
+ * @n: char param
+ * @num: last param
+ *
+ * Return: length of string
+ */
+
+int  print_num(char *n, int num)
+{
+	n = malloc(sizeof(int));
+
+	sprintf(n, "%d", num);
+
+	n, strlen(n);
+
+	return (strlen(n));
+}
